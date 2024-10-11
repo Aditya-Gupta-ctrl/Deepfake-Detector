a637be610ccbd4b27aa727d4409eb35684aa82be
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-10-11 10:48:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ with st.sidebar:
             ]),
         ], size='lg', variant='left-bar', color='grape', open_all=True, return_index=True)
     
-if selected == 1:
+if selected == 0:
         # Create a Streamlit app
         st.title("Image and Video Input")
         
